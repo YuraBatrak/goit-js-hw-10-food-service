d131848622be329b8c7335d19c645149a010e5fd
YuraBatrak <yurabatrak@gmail.com>
2021-10-07 20:43:19 +0300

    Update menu.js

--- src/partials/menu.js
+++ src/partials/menu.js
@@ -1,11 +1,26 @@
 import menuTeplate from "./menu.hbs";
 import menu from './menu.json';
-
-// const Theme = {
-//   LIGHT: 'light-theme',
-//   DARK: 'dark-theme',
-// };
 const menuRef = document.querySelector('.js-menu');
-// console.log(menuRef);
 const menuSample = menuTeplate(menu);
 menuRef.insertAdjacentHTML("beforeend", menuSample);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const input = document.querySelector('#theme-switch-toggle');
+document.body.classList.add(localStorage.getItem('theme') 
+  ? localStorage.getItem('theme') : Theme.LIGHT,);
+if (document.body.classList.value === Theme.DARK) {
+  input.checked = true;
+}
+input.addEventListener('change', e => {
+  if (e.target.checked) {
+    localStorage.setItem('theme', Theme.DARK);
+    document.body.classList.remove(Theme.LIGHT);
+    document.body.classList.add(Theme.DARK);
+  } else {
+    localStorage.setItem('theme', Theme.LIGHT);
+    document.body.classList.remove(Theme.DARK);
+    document.body.classList.add(Theme.LIGHT);
+  }
+});
